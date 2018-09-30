f3934348728221ae1b1899c78cd085205c0f6f30
hmc123321 <37052560+hmc123321@users.noreply.github.com>
2018-09-30 08:37:49 +0800

    Add files via upload

---
+++ _74hc595.ino
@@ -0,0 +1,53 @@
+#define DS 9
+#define STCP 11
+#define SHCP 10
+#define OE 12
+#define MR 2
+#define ulong unsigned long 
+#define uchar unsigned char 
+#define uint unsigned int 
+void hc595_16 (uint data);
+uint ctl(uchar h,uchar l);
+void setup() {
+  pinMode (SHCP,1);
+  pinMode (STCP,1);
+  pinMode (DS,1);
+  pinMode (MR,1);
+  pinMode (OE,1);
+  digitalWrite (OE,LOW);
+  digitalWrite (MR,HIGH);
+}
+void loop() {
+   hc595_16(cti(0x7f,0xfe));
+   while (1);
+}
+void hc595_16 (uint data){
+  digitalWrite (STCP,LOW);
+  for (int i=0;i<16;i++){
+    digitalWrite (SHCP,LOW);
+    digitalWrite (DS,data>>15);
+    digitalWrite (SHCP,HIGH);
+    data<<=1;
+    }
+  digitalWrite (STCP,HIGH);
+  digitalWrite (STCP,LOW);
+}
+
+void hc595_8 (uchar data){
+  int i=0;
+  digitalWrite (STCP,LOW);
+  for (;i<8;i++){
+    digitalWrite (SHCP,LOW);
+    digitalWrite (DS,data>>7);
+    digitalWrite (SHCP,HIGH);
+    data<<=1;
+    }
+  digitalWrite (STCP,HIGH);
+  digitalWrite (STCP,LOW);
+}
+
+uint cti(uchar h,uchar l){
+  uint data;
+  data=(h/0x10)*0x1000+(h%0x10)*0x100+l;
+  return data;
+}
